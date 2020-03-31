ec2119d42b237ed62cb7c85a42b01b9de3d3becf
hemantdevfactory <hemant.shinde@devfactory.com>
2020-03-31 13:03:02 +0530

    Fixed

--- res_leak2.java
+++ res_leak2.java
@@ -1,13 +1,16 @@
 import java.sql.*;  
 class MysqlCon1{  
    public static void main1(String args[]){  
+         Connection con=null;
+         Statement stmt=null;
+         ResultSet rs=null;
       try{  
          Class.forName("com.mysql.jdbc.Driver");  
-         Connection con=DriverManager.getConnection(  
+         con=DriverManager.getConnection(  
                "jdbc:mysql://localhost:3306/sonoo","root","root");  
          //here sonoo is database name, root is username and password  
-         Statement stmt=con.createStatement();  
-         ResultSet rs=stmt.executeQuery("select * from emp");  
+         stmt=con.createStatement();  
+         rs=stmt.executeQuery("select * from emp");  
          while(rs.next())  
             System.out.println(rs.getInt(1)+"  "+rs.getString(2)+"  "+rs.getString(3));  
          //con.close();  
