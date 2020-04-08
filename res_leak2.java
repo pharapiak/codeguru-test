c105ece0db4a61f8aa0e68cc6ec9552faf1e2fdd
hemantdevfactory <hemant.shinde@devfactory.com>
2020-04-08 15:09:31 +0530

    one resource leak

--- res_leak2.java
+++ res_leak2.java
@@ -17,11 +17,11 @@ class MysqlCon1{
       }catch(Exception e){ System.out.println(e);
          if(con!=null) { con.close();}
          if(stmt!=null) { stmt.close();}
-         if(rs!=null) { rs.close();}
+         //if(rs!=null) { rs.close();}
       }finally {
          if(con!=null) { con.close();}
          if(stmt!=null) { stmt.close();}
-         if(rs!=null) { rs.close();}
+         //if(rs!=null) { rs.close();}
       }  
    }  
 }  
