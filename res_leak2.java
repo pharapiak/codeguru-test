f907c196191cd1ac27840d07aa7c0458f265e78c
hemantdevfactory <hemant.shinde@devfactory.com>
2020-04-08 16:17:12 +0530

    Added one more resource leak, to the existing resource leak

--- res_leak2.java
+++ res_leak2.java
@@ -15,11 +15,11 @@ class MysqlCon1{
             System.out.println(rs.getInt(1)+"  "+rs.getString(2)+"  "+rs.getString(3));  
          //con.close();  
       }catch(Exception e){ System.out.println(e);
-         if(con!=null) { con.close();}
+         //if(con!=null) { con.close();}
          //if(stmt!=null) { stmt.close();}
          //if(rs!=null) { rs.close();}
       }finally {
-         if(con!=null) { con.close();}
+         //if(con!=null) { con.close();}
          //if(stmt!=null) { stmt.close();}
          //if(rs!=null) { rs.close();}
       }  
