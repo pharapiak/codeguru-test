018e4beda17712a218c591fa9bc0054592a6d628
hemantdevfactory <hemant.shinde@devfactory.com>
2020-04-08 12:28:14 +0530

    stray text after ;

--- res_leak7.java
+++ res_leak7.java
@@ -22,7 +22,7 @@ class MysqlCon7{
          int num19,
          int num20
          ){ 
-            return num1+num2+num3+num4+num5+num6+num7+num8+num9+num10+num11+num12+num13+num14+num15+num16+num17+num18+num19+num20
+            return num1+num2+num3; +num4+num5+num6+num7+num8+num9+num10+num11+num12+num13+num14+num15+num16+num17+num18+num19+num20
          }  
    public static void main7(String args[]){  
       try{  
