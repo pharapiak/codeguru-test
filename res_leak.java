b515786ba8f27a90bba29d4d2ceb1f94cd538229
hemantdevfactory <hemant.shinde@devfactory.com>
2020-04-11 22:10:27 +0530

    fixed unrelated issue to check if Reviwer only reviews the changed lines

--- res_leak.java
+++ res_leak.java
@@ -9,7 +9,7 @@ class MysqlCon{
          Statement stmt=con.createStatement();  
          ResultSet rs=stmt.executeQuery("select * from emp");  
          while(rs.next())  
-            System.out.println(rs.getInt(1)+"  "+rs.getString(2)+"  "+rs.getString(3));  
+            System.out.println(rs.getInt(1); //+"  "+rs.getString(2)+"  "+rs.getString(3));  
          //con.close();  
       }catch(Exception e){ System.out.println(e);}  
    }  
