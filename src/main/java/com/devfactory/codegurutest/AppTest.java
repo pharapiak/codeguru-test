976600bcefce4f7b0ee1b9c7ac2698e99404f143
Paul Harapiak <6496259+pharapiak@users.noreply.github.com>
2020-04-28 19:54:28 -0400

    cleaned 2 resource leaks

--- src/main/java/com/devfactory/codegurutest/AppTest.java
+++ src/main/java/com/devfactory/codegurutest/AppTest.java
@@ -6,43 +6,25 @@ class AppTest {
       Statement stmt = null;
       ResultSet rs = null;
       try {
+         Class.forName("com.mysql.jdbc.Driver");
+         con = DriverManager.getConnection("jdbc:mysql://localhost:3306/sonoo", "root", "root");
+         // here sonoo is database name, root is username and password
+         stmt = con.createStatement();
+         rs = stmt.executeQuery("select * from emp");
+         while (rs.next())
+            System.out.println(rs.getInt(1) + "  " + rs.getString(2) + "  " + rs.getString(3));
+         // con.close();
+      }catch (Exception ignore){
+      } finally {
          try {
-            Class.forName("com.mysql.jdbc.Driver");
-            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/sonoo", "root", "root");
-            // here sonoo is database name, root is username and password
-            stmt = con.createStatement();
-            rs = stmt.executeQuery("select * from emp");
-            while (rs.next())
-               System.out.println(rs.getInt(1) + "  " + rs.getString(2) + "  " + rs.getString(3));
-            // con.close();
-         } catch (Exception e) {
-            System.out.println(e);
-            if (con != null) {
-               con.close();
-            }
-            if (stmt != null) {
-               stmt.close();
-            }
-            // if(rs!=null) { rs.close();}
-         } finally {
-            if (con != null) {
-               con.close();
-            }
-            if (stmt != null) {
-               stmt.close();
-            }
-            // if(rs!=null) { rs.close();}
-         }
-      } catch (SQLException e) {
-         System.out.println(e);
-         if (con != null) {
-            con.close();
-         }
-         if (stmt != null) {
             stmt.close();
+         } catch (Exception ignore) {
          }
-         // if(rs!=null) { rs.close();}
-
+         try {
+            con.close();
+         } catch (Exception ignore) {
+         }
+         // try {rs.close();} catch (Exception ignore){};
       }
    }
 }
